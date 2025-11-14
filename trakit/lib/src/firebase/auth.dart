5f384ce72bed76f7a2c3ee0e20a6c7ca28230a9b
maynornavarrete29-code <maynornavarrete29@gmail.com>
2025-11-13 18:05:30 -0600

    se empezo a configurar inicio de sesion con google

---
+++ trakit/lib/src/firebase/auth.dart
@@ -0,0 +1,21 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<UserCredential?> signInWithGoogle() async {
+  // Trigger the authentication flow
+    final GoogleSignIn signIn = GoogleSignIn.instance;
+
+    await signIn.initialize();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAccount googleAuth = await signIn.authenticate();
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      // accessToken: ,
+      idToken: googleAuth.authentication.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+}
